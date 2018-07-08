4501f03859aaeef097c5a342d46ebf6487184c5d
Mishiwata01 <bscbsc00@gmail.com>
2018-07-08 15:32:28 +0900

    create redmine dir

---
+++ apps/redmine/htdocs/config/thin_service_envvars.rb
@@ -0,0 +1,5 @@
+ENV['PATH'] = 'C:\Bitnami\redmine-3.4.5-0/git\bin;' + ENV['PATH']
+ENV['PATH'] = 'C:\Bitnami\redmine-3.4.5-0/ruby\bin;' + ENV['PATH']
+ENV['PATH'] = 'C:\Bitnami\redmine-3.4.5-0/DevKit\bin;' + ENV['PATH']
+ENV['PATH'] = 'C:\Bitnami\redmine-3.4.5-0\imagemagick;' + ENV['PATH']
+ENV['MAGICK_CODER_MODULE_PATH'] = 'C:\Bitnami\REDMIN~1.5-0\IMAGEM~1\modules\coders'
